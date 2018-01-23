a18918593cd43c5d89b96ebd078a42c0c1f7557a
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-23 00:04:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musician, instrument) {
   var pairs = [];
-  for (var i = -1; i <= musician.length; i++) {
-    pairs.unshift(`${musician[i]} plays ${instrument[i]}`);
+  for (var i = 0; i <= musician.length; i++) {
+    pairs.push(`${musician[i]} plays ${instrument[i]}`);
     return pairs;
   }
 }
